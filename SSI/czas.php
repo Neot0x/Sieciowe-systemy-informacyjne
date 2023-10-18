09756ee56659b0e6e874caf597c4d61ab626bb0c
Neot0x <57873348+Neot0x@users.noreply.github.com>
2023-10-18 21:21:37 +0200

    Poprawka do zajęć z 12.10.2023

---
+++ SSI/czas.php
@@ -0,0 +1,4 @@
+<?php 
+date_default_timezone_set("Europe/Warsaw");
+echo 'Jest teraz: '.date("Y/m/d h:i:s a", time());
+?>
---
+++ SSI/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Monkey Space</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styl.css">
+		<link rel="icon" type="image/x-icon" href="banan.png">
+    </head>
+    <body>
+        <header><h1>Monkey Space<img src="monkey_logo.jpg" alt="Logo bloga"></h1>
+                    
+        </header>
+        <div class = "row">
+             <div class = "column left">Główna sekcja
+                <p><b>Małpokształtne, małpy właściwe (Simiiformes)</b> – infrarząd ssaków naczelnych z podrzędu wyższych naczelnych obejmujący małpy szerokonose i małpy wąskonose.
+				</br>
+				a
+				</br>
+				a
+				</br>
+				a
+				</br>
+				a
+                </p>
+             </div>
+             <div class = "column right">Pasek boczny
+                <ol>
+                    <li><a href = #>O mnie</a></li>
+                    <li><a href = #>Kalendarz</a></br>
+						<?php include 'czas.php';
+						?>
+					</li>
+                    <li><a href = #>Archiwum</a></li>
+                    <li><a href = #>Linki do stron</a></li>
+                    <li><a href = #>Panel administratorów</a></li>
+                </ol>
+             </div>
+        </div>
+        <footer> Stworzone przez Mateusz Białek </footer>
+    </body>
+</html>
